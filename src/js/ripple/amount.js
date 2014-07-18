a6fce260a5e758146c4b32ab0dff3a64169b216a
Scott Fleckenstein <nullstyle@gmail.com>
2014-07-18 08:56:50 -0700

    Don't round amounts up when to_human'ing them, truncate them

--- src/js/ripple/amount.js
+++ src/js/ripple/amount.js
@@ -1075,9 +1075,6 @@ Amount.prototype.to_human = function(opts) {
   if (fraction_part.length || !opts.skip_empty_fraction) {
     // Enforce the maximum number of decimal digits (precision)
     if (typeof opts.precision === 'number') {
-      if (opts.precision === 0 && fraction_part.charCodeAt(0) >= 53) {
-        int_part = (Number(int_part) + 1).toString();
-      }
       fraction_part = fraction_part.slice(0, opts.precision);
     }
 
